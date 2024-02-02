43de6b1d9fdaa62b33b8c618f206ccf47591a977
abelhoula <ahmd.belhoula@gmail.com>
2024-02-02 21:25:39 +0100

    scaffolded the project & created pilot api objects
    Signed-off-by: abelhoula <ahmd.belhoula@gmail.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+![Tofan](docs/imgs/tofan-io.png "Tofan")
+
+Tofan is a Kubernetes operator designed to automate and streamline the performance testing of Kubernetes controllers. It dynamically manages resource creation, orchestrates test workflows, and aggregates metrics directly from target controllers, providing comprehensive reports on performance outcomes. Built for integration and scalability, Tofan offers a declarative approach to ensure your cluster components are tested thoroughly and efficiently.
+## License
+
+Copyright 2024 invioteq llc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
---
+++ api/v1alpha1/testcase_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2024 invioteq llc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// TestCaseSpec defines the desired state of TestCase
+type TestCaseSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of TestCase. Edit testcase_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// TestCaseStatus defines the observed state of TestCase
+type TestCaseStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// TestCase is the Schema for the testcases API
+type TestCase struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   TestCaseSpec   `json:"spec,omitempty"`
+	Status TestCaseStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// TestCaseList contains a list of TestCase
+type TestCaseList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []TestCase `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&TestCase{}, &TestCaseList{})
+}
