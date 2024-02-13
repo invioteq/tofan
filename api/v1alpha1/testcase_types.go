50207b8b2a5f70f376e2aa5810634cdce177c992
abelhoula <ahmd.belhoula@gmail.com>
2024-02-13 08:39:48 +0100

    feat(testCase-readiness-criteria): add generic ReadinessCriteria in testcase api spec
    Signed-off-by: abelhoula <ahmd.belhoula@gmail.com>

--- api/v1alpha1/testcase_types.go
+++ api/v1alpha1/testcase_types.go
@@ -26,37 +26,56 @@ import (
 
 // TestCaseSpec defines the desired state of TestCase
 type TestCaseSpec struct {
-	// Reference to a ObjectTemplate
+	// ObjectTemplateRef is a reference to an ObjectTemplate resource
 	ObjectTemplateRef objectTemplateReference `json:"objectTemplateRef,omitempty"`
-	// Action specifies the operation to perform with the ObjectTemplate (e.g., create, delete)
+	// Action specifies the operation to perform on the objectTemplate, e.g., create
 	Action string `json:"action,omitempty"`
-	// Count specifies the number of instances to create/delete
+	// Count specifies the number of instances to create
 	Count int `json:"count"`
-	// Concurrency specifies how many operations can be performed concurrently
+	// Concurrency controls how many instances can be created concurrently
 	Concurrency int `json:"concurrency"`
-	// DynamicFields specifies how to dynamically set fields in the ObjectTemplate based on the test case.
+	// DynamicFields allow specifying dynamic values for certain fields in the object template
 	DynamicFields []DynamicField `json:"dynamicFields,omitempty"`
+	// ReadinessCriteria specifies the conditions that must be met for the resources to be considered ready
+	ReadinessCriteria []ResourceReadinessCriteria `json:"readinessCriteria"`
 	// TargetMetrics defines the metrics that should be collected during the test
 	TargetMetrics []MetricTarget `json:"targetMetrics,omitempty"`
 }
 
 // DynamicField defines a field to dynamically set based on TestCase parameters.
 type DynamicField struct {
-	// Path specifies the JSON path to the field within the ObjectTemplate that needs to be dynamically set.
+	// Path is the JSON path to the field within the object template that needs a dynamic value.
 	Path string `json:"path"`
-
-	// Values are the values to apply to the dynamic field as simple strings.
+	// Values are the dynamic values to apply to the specified path.
 	Values map[string]extv1.JSON `json:"values"`
 }
 
 // objectTemplateReference
 type objectTemplateReference struct {
-	// Name of the ObjectTemplate.
+	// Name is the name of the ObjectTemplate resource
 	Name string `json:"name,omitempty"`
 	// Kind specifies the kind of the referenced resource, which should be "ObjectTemplate".
 	Kind string `json:"kind,omitempty"`
 	// Group is the API group of the SpaceTemplate,  "tofan.io/v1alpha1".
 	Group string `json:"group,omitempty"`
+}
+
+// ResourceReadinessCriteria defines readiness criteria for a specific resource type
+type ResourceReadinessCriteria struct {
+	// ResourceType is the kind of resource to check for readiness, e.g., Deployment, CustomResource
+	ResourceType string `json:"resourceType"`
+	// Conditions are the specific conditions to be met for the resource to be considered ready
+	Conditions []ResourceCondition `json:"conditions"`
+}
+
+// ResourceCondition defines a single condition to check on a resource for readiness
+type ResourceCondition struct {
+	// Type is the type of condition, e.g., Available for Deployments, Ready for custom resources
+	Type string `json:"type"`
+	// Status is the status value the condition must have, e.g., "True", "False"
+	Status string `json:"status"`
+	// JSONPath (optional) is a JSON path expression to locate the condition value within a custom resource status
+	JSONPath string `json:"jsonPath,omitempty"`
 }
 
 // MetricTarget defines a target metric for collection by the testCase
